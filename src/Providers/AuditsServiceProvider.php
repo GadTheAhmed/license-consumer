9a77b7b4e43153ee8a7b7584e93759f911149292
Ahmed Gad <eng.ahmedmgad@gmail.com>
2024-01-08 11:59:16 +0200

    first commit

---
+++ src/Providers/AuditsServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Laravel\Audit\Providers;
+
+use Closure;
+use Illuminate\Support\ServiceProvider;
+
+class AuditServiceProvider extends ServiceProvider
+{
+
+    public function register(){}
+
+    public function boot(){}
+}
